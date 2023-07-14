7b2fc6c7920a365b2936c88ca1c2dbf00e6a152c
Konrad Gruzło <gruzlokonrad@gmail.com>
2023-07-14 20:53:54 +0200

    update env name

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    GOOGLE_TAG_MANAGER: "GTM-MRKPTZ2",
+    NEXT_PUBLIC_GOOGLE_TAG_MANAGER: "GTM-MRKPTZ2",
     MONGODB_URI: "mongodb+srv://project3_database:pass123@cluster0.axyp0.mongodb.net/adomi"
   }
 }
